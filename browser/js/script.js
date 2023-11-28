a8e7dc93925235b2b4556f74e03f459cddd41dfa
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2023-11-28 12:06:35 +0100

    Avanzamento snack

---
+++ browser/js/script.js
@@ -0,0 +1,19 @@
+/*Un oggetto rappresenta una finestra di un browser e contiene due campi: un array di tab aperte e un numero che indica l'indice della tab aperta nell'array:
+{
+    "tab": ["Facebook", "GitHub", "Gmail"],
+    "activeTab": 0
+}
+Il software deve guardare se c'è un social aperto ed eliminarlo dalle tab.
+Nel caso la tab fosse attiva, deve attivare la successiva.*/
+
+const social = {
+    tab = [
+    "Facebook",
+    "Github",
+    "Gmail",
+    "Youtube",
+    "Instagram"]
+    activeTab: 0
+};
+
+
---
+++ map/js/script.js
@@ -0,0 +1,65 @@
+/*Crea un array di oggetti che rappresentino delle persone.
+Ogni persona ha un nome, un cognome e un'età.
+
+Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.*/
+
+const people = [
+    {
+        name: "Aldo",
+        surname: "Baglio",
+        age: "40"
+    },
+    {
+        name: "Giovanni",
+        surname: "Storti",
+        age: "55"
+    },
+    {
+        name: "Giacomo",
+        surname: "Poretti",
+        age: "70"
+    },
+    {
+        name: "Giovanna",
+        surname: "Pozzi",
+        age: "14"
+    },
+    {
+        name: "Giorgia",
+        surname: "Valsecchi",
+        age: "17"
+    },
+    {
+        name: "Nunzio",
+        surname: "Russo",
+        age: "18"
+    },
+    {
+        name: "Valerio",
+        surname: "Massimino",
+        age: "27"
+    },
+    {
+        name: "Marco",
+        surname: "Mosca",
+        age: "7"
+    },
+    {
+        name: "Isacco",
+        surname: "Brambilla",
+        age: "99"
+    },
+    {
+        name: "Ugo",
+        surname: "Fantozzi",
+        age: "80"
+    }];
+
+    if (people.age < 18){
+        return minor
+    };
+
+const newArray = people.map ((singlePerson) =>{
+    singlePerson.comeCompleto =singlePerson.name
+}
+)
